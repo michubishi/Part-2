2d7345ea2824fe24f02421c5a329c151299e8447
vuomiche <vuomiche@sheridancollege.ca>
2024-02-01 13:53:39 -0500

    task 1 complete: draw flight path

---
+++ Assets/Week 4/Scripts/Plane.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Plane : MonoBehaviour
+{
+    public List<Vector2> points;
+    public float newPointThreshold = 0.2f;
+    Vector2 lastPosition;
+    LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0, transform.position);
+    }
+
+    private void OnMouseDown()
+    {
+        points = new List<Vector2>();
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0, transform.position);
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if(Vector2.Distance(newPosition, lastPosition) > newPointThreshold)
+        {
+            points.Add(newPosition);
+            lineRenderer.positionCount++;
+            lineRenderer.SetPosition(lineRenderer.positionCount - 1, newPosition);
+            lastPosition = newPosition;
+        }
+
+    }
+
+}
