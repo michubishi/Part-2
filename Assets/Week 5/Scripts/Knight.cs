2c32d4462c63c17dced8ef244c59f55cb7479574
vuomiche <vuomiche@sheridancollege.ca>
2024-02-08 14:23:53 -0500

    Task 1 complete: player gets hurt on click

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -9,6 +9,7 @@ public class Knight : MonoBehaviour
     public float speed = 3;
     Rigidbody2D rb;
     Animator animator;
+    bool clickingOnSelf = false;
 
     void Start()
     {
@@ -31,11 +32,22 @@ public class Knight : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !clickingOnSelf)
         {
             destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
 
         animator.SetFloat("Movement", movement.magnitude);
+    }
+
+    private void OnMouseDown()
+    {
+        clickingOnSelf = true;
+        animator.SetTrigger("TakeDamage");
+    }
+
+    private void OnMouseUp()
+    {
+        clickingOnSelf = false;
     }
 }
