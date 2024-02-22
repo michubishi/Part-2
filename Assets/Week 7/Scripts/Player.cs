621830a9df25db829238233cc0d88434f55fb634
vuomiche <vuomiche@sheridancollege.ca>
2024-02-22 14:22:15 -0500

    Task 1b complete: mouse can select players

---
+++ Assets/Week 7/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+
+public class Player : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    public Color selectedColor;
+    public Color unselectedColor;
+    bool isSelected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Selected(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Selected(true);
+    }
+
+    public void Selected(bool isSelected)
+    {
+        if (isSelected)
+        {
+            spriteRenderer.color = selectedColor;
+        }
+
+        else
+        {
+            spriteRenderer.color = unselectedColor;
+        }
+    }
+
+}
